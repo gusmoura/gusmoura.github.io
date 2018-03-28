4492c1638c938b3977986d5c1050725bb4a32cbe
Gustavo Moura <gustavolpmoura@gmail.com>
2018-03-27 23:17:36 -0300

    Add files via upload
    Todo List web app

---
+++ assets/js/todos.js
@@ -0,0 +1,31 @@
+// Check off Specif Todos By Clicking
+$("ul").on("click", "li", function(){
+	$(this).toggleClass("completed");
+});
+
+//Click on X to delete Todos
+$("ul").on("click", "span", function(event){
+	//remove the parent element with an fadeOut effect
+	$(this).parent().fadeOut(500,function(){
+		$(this).remove();
+	});
+	event.stopPropagation();
+});
+
+//key press
+$("input[type='text']").keypress(function(event){
+	if(event.which === 13){
+		//grabbing new todo text from input
+		var todoText = $(this).val();
+		//clear input
+		$(this).val("");
+
+		//create a new li and add to ul
+		$("ul").append("<li><span><i class='fa fa-trash'></i> </span>" + todoText + "</li>");
+	}
+});
+
+$(".fa-plus").click(function(){
+	$("input[type='text'").fadeToggle();
+})
+
